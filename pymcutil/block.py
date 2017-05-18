760730943464ee125294b899ec828b84a5bbb90f
Arcensoth <arcensoth@gmail.com>
2017-05-18 05:14:22 -0400

    Allow block state to be provided as any mapping

--- pymcutil/block.py
+++ pymcutil/block.py
@@ -1,4 +1,5 @@
 import abc
+from collections import Mapping
 
 from pymcutil.block_state import BlockState
 
@@ -6,6 +7,6 @@ from pymcutil.block_state import BlockState
 class Block(abc.ABC):
     """ Represents a Minecraft block. """
 
-    def __init__(self, block_id: str, block_state: BlockState = None):
+    def __init__(self, block_id: str, block_state: Mapping = None):
         self.block_id: str = block_id
-        self.block_state: BlockState = block_state if block_state is not None else BlockState()
+        self.block_state: BlockState = BlockState(**(block_state or {}))
--- tests/test_block.py
+++ tests/test_block.py
@@ -5,7 +5,19 @@ from pymcutil.block_state import BlockState
 
 
 class BlockTestCase(unittest.TestCase):
-    def test_block_with_state(self):
+    def test_block_with_id(self):
+        b = Block('wool')
+        self.assertEqual(b.block_id, 'wool')
+
+    def test_block_with_id_and_block_state(self):
         b = Block('wool', BlockState(color='red'))
         self.assertEqual(b.block_id, 'wool')
+        self.assertEqual(b.block_state, BlockState(color='red'))
+
+    def test_block_state_from_dict(self):
+        b = Block('wool', dict(color='red'))
+        self.assertEqual(b.block_state, BlockState(color='red'))
+
+    def test_block_state_from_braces(self):
+        b = Block('wool', {'color': 'red'})
         self.assertEqual(b.block_state, BlockState(color='red'))
