8ace299c3f6a01848a208f4bd35bbfad46c0f430
Arcensoth <arcensoth@gmail.com>
2017-05-18 04:55:10 -0400

    Remove block tests involving kwargs

--- tests/test_block.py
+++ tests/test_block.py
@@ -9,13 +9,3 @@ class BlockTestCase(unittest.TestCase):
         b = Block('wool', BlockState(color='red'))
         self.assertEqual(b.block_id, 'wool')
         self.assertEqual(b.block_state, BlockState(color='red'))
-
-    def test_block_with_kwargs(self):
-        b = Block('slab', variant='quartz')
-        self.assertEqual(b.block_id, 'slab')
-        self.assertEqual(b.block_state, BlockState(variant='quartz'))
-
-    def test_block_with_state_and_kwargs(self):
-        b = Block('slab', BlockState(half='bottom'), variant='quartz')
-        self.assertEqual(b.block_id, 'slab')
-        self.assertEqual(b.block_state, BlockState(half='bottom', variant='quartz'))
