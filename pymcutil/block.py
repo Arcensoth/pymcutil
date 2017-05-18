13e623a8958f15a0b33287d1a9fec2297581599e
Arcensoth <arcensoth@gmail.com>
2017-05-18 03:57:49 -0400

    Force block_state arg to be used instead of ambiguous kwargs

--- pymcutil/block.py
+++ pymcutil/block.py
@@ -6,8 +6,6 @@ from pymcutil.block_state import BlockState
 class Block(abc.ABC):
     """ Represents a Minecraft block. """
 
-    def __init__(self, block_id: str, block_state: BlockState = None, **kwargs):
+    def __init__(self, block_id: str, block_state: BlockState = None):
         self.block_id: str = block_id
         self.block_state: BlockState = block_state if block_state is not None else BlockState()
-        for k, v in kwargs.items():
-            self.block_state[k] = v
