c64e88a0b87787aab74e8da3f9b3eb0924d1903d
Arcensoth <arcensoth@gmail.com>
2017-05-19 00:44:30 -0400

    Add block entity tests

---
+++ tests/test_block_entity.py
@@ -0,0 +1,31 @@
+import unittest
+
+from pymcutil.block_entity import BlockEntity
+from pymcutil.block_state import BlockState
+from pymcutil.data_tag.compound_data_tag import CompoundDataTag
+from pymcutil.data_tag.string_data_tag import StringDataTag
+
+
+class BlockTestCase(unittest.TestCase):
+    def test_with_id(self):
+        be = BlockEntity('chest')
+        self.assertEqual(be.block_id, 'chest')
+
+    def test_with_id_and_block_state(self):
+        be = BlockEntity('chest', BlockState(facing='north'))
+        self.assertEqual(be.block_id, 'chest')
+        self.assertEqual(be.block_state['facing'], 'north')
+
+    def test_with_id_and_block_state_and_data_tag(self):
+        be = BlockEntity('chest', BlockState(facing='north'), CompoundDataTag(lock='password'))
+        self.assertEqual(be.block_id, 'chest')
+        self.assertEqual(be.block_state['facing'], 'north')
+        self.assertEqual(be.data_tag['lock'], StringDataTag('password'))
+
+    def test_data_tag_from_dict(self):
+        be = BlockEntity('chest', data_tag=dict(lock='password'))
+        self.assertEqual(be.data_tag['lock'], StringDataTag('password'))
+
+    def test_data_tag_from_braces(self):
+        be = BlockEntity('chest', data_tag={'lock': 'password'})
+        self.assertEqual(be.data_tag['lock'], StringDataTag('password'))
