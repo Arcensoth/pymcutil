1913f8b3d4a19515d3a3c117532fc353131774f0
Arcensoth <arcensoth@gmail.com>
2017-05-19 01:27:18 -0400

    Use byte constants and add tests for min/max values

--- tests/data_tag/test_byte_data_tag.py
+++ tests/data_tag/test_byte_data_tag.py
@@ -1,5 +1,6 @@
 import unittest
 
+from pymcutil.constants import BYTE_MIN, BYTE_MAX
 from pymcutil.data_tag.byte_data_tag import ByteDataTag
 
 
@@ -24,13 +25,23 @@ class ByteDataTagTestCase(unittest.TestCase):
         self.assertEqual(dt.value, 1)
         self.assertEqual(dt.to_str(), '1b')
 
+    def test_min(self):
+        dt = ByteDataTag(BYTE_MIN)
+        self.assertEqual(dt.value, BYTE_MIN)
+        self.assertEqual(dt.to_str(), f'{BYTE_MIN}s')
+
+    def test_max(self):
+        dt = ByteDataTag(BYTE_MAX)
+        self.assertEqual(dt.value, BYTE_MAX)
+        self.assertEqual(dt.to_str(), f'{BYTE_MAX}s')
+
     def test_too_low(self):
         with self.assertRaises(ValueError):
-            ByteDataTag(-129)
+            ByteDataTag(BYTE_MIN - 1)
 
     def test_too_high(self):
         with self.assertRaises(ValueError):
-            ByteDataTag(128)
+            ByteDataTag(BYTE_MAX + 1)
 
     def test_equality(self):
         dt1 = ByteDataTag(1)
