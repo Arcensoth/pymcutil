27389505026ba704337cb5105dc1960b20299d6b
Arcensoth <arcensoth@gmail.com>
2017-05-27 15:36:18 -0400

    Use sifting for command arguments

--- pymcutil/command/commands/scoreboard/players/tag/add.py
+++ pymcutil/command/commands/scoreboard/players/tag/add.py
@@ -1,7 +1,8 @@
-from typing import Union
+from typing import Union, Mapping
 
 from pymcutil.command.target import Target
 from pymcutil.data_tag import DataTag
+from pymcutil.data_tag.compound_data_tag import CompoundDataTag
 from ..tag_ import ScoreboardPlayersTagCommand
 
 CMD = 'add'
@@ -16,15 +17,14 @@ class ScoreboardPlayersTagAddCommand(ScoreboardPlayersTagCommand):
     http://minecraft.gamepedia.com/Scoreboard#Tags_commands
     """
 
-    def __init__(self, target: Target, tag: str, data_tag: DataTag = None):
+    def __init__(self, target: Target, tag: str, data_tag: Mapping = None):
         super().__init__(target)
         self.tag: str = tag
-        self.data_tag: Union[DataTag, None] = data_tag
+        self.data_tag: Union[CompoundDataTag, None] = CompoundDataTag.sift(data_tag, None)
 
     def params(self):
         yield from super().params()
         yield from (CMD, self.tag)
-
         if self.data_tag is not None:
             yield self.data_tag
 
