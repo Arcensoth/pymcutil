061b07612a3dc64b9a589360727ac41f8c990e51
Arcensoth <arcensoth@gmail.com>
2017-05-28 18:16:22 -0400

    Cleanup scoreboard command tests

--- tests/command/commands/scoreboard/players/tag/test_remove.py
+++ tests/command/commands/scoreboard/players/tag/test_remove.py
@@ -5,18 +5,16 @@ from pymcutil.data_tag.compound_data_tag import CompoundDataTag
 from pymcutil.selector import selectors
 
 
-class ScoreboardPlayersTagAddCommandTestCase(unittest.TestCase):
+class ScoreboardPlayersTagRemoveCommandTestCase(unittest.TestCase):
     def test_without_data_tag(self):
-        self.assertEqual(
-            str(commands.scoreboard.players.tag.remove(
-                target=selectors.PLAYER,
-                tag='some_tag')),
-            'scoreboard players tag @p remove some_tag')
+        cmd = commands.scoreboard.players.tag.remove(
+            target=selectors.PLAYER,
+            tag='some_tag')
+        self.assertEqual(str(cmd), 'scoreboard players tag @p remove some_tag')
 
     def test_with_data_tag(self):
-        self.assertEqual(
-            str(commands.scoreboard.players.tag.remove(
-                target=selectors.PLAYER,
-                tag='on_ground',
-                data_tag=CompoundDataTag({'OnGround': False}))),
-            'scoreboard players tag @p remove on_ground {OnGround:0b}')
+        cmd = commands.scoreboard.players.tag.remove(
+            target=selectors.PLAYER,
+            tag='on_ground',
+            data_tag=CompoundDataTag({'OnGround': False}))
+        self.assertEqual(str(cmd), 'scoreboard players tag @p remove on_ground {OnGround:0b}')
