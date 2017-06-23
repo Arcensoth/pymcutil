8e8bc57ac97a037b71420f403679c4fc7ff06862
Arcensoth <arcensoth@gmail.com>
2017-06-23 16:03:27 -0400

    Rename path_components to just components

--- pymcutil/util/resources.py
+++ pymcutil/util/resources.py
@@ -4,33 +4,33 @@ from typing import Iterable
 from pymcutil.constants.resources import *
 
 
-def get_resource_trail(path_components: Iterable) -> str:
-    return '/'.join(path_components)
+def get_resource_trail(components: Iterable) -> str:
+    return '/'.join(components)
 
 
-def get_resource_name(namespace: str, path_components: Iterable) -> str:
-    return '{}:{}'.format(namespace, get_resource_trail(path_components))
+def get_resource_name(namespace: str, components: Iterable) -> str:
+    return '{}:{}'.format(namespace, get_resource_trail(components))
 
 
 def get_resource_path(
         root: str, resource_dir: str, namespace: str, resource_subdir: str, resource_ext: str,
-        path_components: Iterable) -> str:
-    return '{}.{}'.format(os.path.join(root, resource_dir, namespace, resource_subdir, *path_components), resource_ext)
+        components: Iterable) -> str:
+    return '{}.{}'.format(os.path.join(root, resource_dir, namespace, resource_subdir, *components), resource_ext)
 
 
-def get_advancement_path(root: str, namespace: str, path_components: Iterable) -> str:
+def get_advancement_path(root: str, namespace: str, components: Iterable) -> str:
     return get_resource_path(
         root=root, resource_dir=DATA_DIRECTORY, namespace=namespace, resource_subdir=ADVANCEMENTS_DIRECTORY,
-        resource_ext=ADVANCEMENTS_EXTENSION, path_components=path_components)
+        resource_ext=ADVANCEMENTS_EXTENSION, components=components)
 
 
-def get_function_path(root: str, namespace: str, path_components: Iterable) -> str:
+def get_function_path(root: str, namespace: str, components: Iterable) -> str:
     return get_resource_path(
         root=root, resource_dir=DATA_DIRECTORY, namespace=namespace, resource_subdir=FUNCTIONS_DIRECTORY,
-        resource_ext=FUNCTIONS_EXTENSION, path_components=path_components)
+        resource_ext=FUNCTIONS_EXTENSION, components=components)
 
 
-def get_loot_table_path(root: str, namespace: str, path_components: Iterable) -> str:
+def get_loot_table_path(root: str, namespace: str, components: Iterable) -> str:
     return get_resource_path(
         root=root, resource_dir=DATA_DIRECTORY, namespace=namespace, resource_subdir=LOOT_TABLES_DIRECTORY,
-        resource_ext=LOOT_TABLES_EXTENSION, path_components=path_components)
+        resource_ext=LOOT_TABLES_EXTENSION, components=components)
