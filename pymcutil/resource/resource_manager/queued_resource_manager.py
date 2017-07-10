d7b8fd65d8f43139a43cb8800f565a9c4ecb64e1
Arcensoth <arcensoth@gmail.com>
2017-07-10 10:18:23 -0400

    Cleanup generate()

--- pymcutil/resource/resource_manager/queued_resource_manager.py
+++ pymcutil/resource/resource_manager/queued_resource_manager.py
@@ -13,6 +13,11 @@ class QueuedResourceManager(ResourceManager):
         self.mapping: Dict[Type[ResourceReference], ResourceGenerator] = dict(mapping)
         self.output_root: str = output_root
 
+    def _locate(self, reference: ResourceReference):
+        generator = self.get_generator(reference)
+        location = generator.location(*reference.params)
+        reference.locate(location)
+
     def set_generator(self, kind: Type[ResourceReference], generator: ResourceGenerator):
         self.mapping[kind] = generator
 
@@ -22,35 +27,28 @@ class QueuedResourceManager(ResourceManager):
         except KeyError:
             raise ResourceReferenceNotMappedError(reference)
 
-    def locate(self, reference: ResourceReference):
-        generator = self.get_generator(reference)
-        location = generator.location(*reference.params)
-        reference.locate(location)
-
     def generate(self, *references: ResourceReference) -> Iterable[ResourcePair]:
         q: Queue = Queue()
 
         # Locate seed refs before anything else.
         for ref in references:
-            self.locate(ref)
+            self._locate(ref)
             q.put(ref)
 
         while not q.empty():
-            resource_reference: ResourceReference = q.get()
-            generator = self.get_generator(resource_reference)
-            resource = generator.generate(*resource_reference.params)
+            reference: ResourceReference = q.get()
+            generator = self.get_generator(reference)
+            resource = generator.generate(*reference.params)
 
             # Locate dependencies before generating output.
             for subref in resource.resource_references:
-                self.locate(subref)
+                self._locate(subref)
                 q.put(subref)
 
-            resource_location = resource_reference.location
-            resource_path = os.path.join(self.output_root, resource_location.path)
+            yield resource, reference.location
 
+            # TODO refactor duplicate code
+            resource_path = os.path.join(self.output_root, reference.location.path)
             os.makedirs(os.path.dirname(resource_path), exist_ok=True)
-
             with open(resource_path, 'w') as resource_file:
                 resource_file.write('\n'.join([str(line) for line in resource.resource_lines] + ['']))
-
-            yield resource, resource_location
