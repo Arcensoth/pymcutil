23a79d2a7449f9912469d9f8139b6927c88435de
Arcensoth <arcensoth@gmail.com>
2017-07-12 13:42:57 -0400

    Reintroduce return types for implemented methods of Resource

--- pymcutil/resource/function.py
+++ pymcutil/resource/function.py
@@ -2,6 +2,7 @@ from typing import Iterable, List
 
 from pymcutil.command.command import Command
 from pymcutil.resource.abc.resource import Resource
+from pymcutil.resource.resource_reference.abc.resource_reference import ResourceReference
 from pymcutil.resource.resource_referent.abc.resource_referent import ResourceReferent
 
 
@@ -10,16 +11,16 @@ class Function(Resource):
         self.commands: List[Command] = list(commands)
 
     @property
-    def direct_resource_references(self):
+    def direct_resource_references(self) -> Iterable[ResourceReference]:
         # A function does not directly reference any resources, only commands.
         yield from ()
 
     @property
-    def resource_referents(self):
+    def resource_referents(self) -> Iterable[ResourceReferent]:
         # Yield all commands that reference resources.
         yield from (command for command in self.commands if isinstance(command, ResourceReferent))
 
     @property
-    def resource_lines(self):
+    def resource_lines(self) -> Iterable:
         # Yield commands to be printed one per line.
         yield from self.commands
