e102cab487e09375f2ff8033b6ebb4da95e9505a
Arcensoth <arcensoth@gmail.com>
2017-08-22 01:11:58 -0400

    Add missing subcommands to commands.execute module

--- pymcutil/command/commands/execute/__init__.py
+++ pymcutil/command/commands/execute/__init__.py
@@ -1,1 +1,5 @@
 from . import if_, store, unless
+from .execute_as_command import as_
+from .execute_at_command import at
+from .execute_offset_command import offset
+from .execute_then_command import then
