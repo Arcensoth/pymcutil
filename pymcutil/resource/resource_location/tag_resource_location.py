00fc34dd20ce8e9f091f25429d29d866daba0762
Arcensoth <arcensoth@gmail.com>
2017-12-07 20:30:58 -0500

    Tag resource locations are denoted by hashtags

--- pymcutil/resource/resource_location/tag_resource_location.py
+++ pymcutil/resource/resource_location/tag_resource_location.py
@@ -6,3 +6,6 @@ from pymcutil.resource.resource_location.standard_resource_location import Stand
 class TagResourceLocation(StandardResourceLocation):
     def __init__(self, namespace: str, components: typing.Iterable[str]):
         super().__init__(namespace=namespace, components=components, subfolder='tags')
+
+    def __str__(self):
+        return '#{}'.format(super().__str__())
