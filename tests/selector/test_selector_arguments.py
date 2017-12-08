f4dc144b72ae76b3ac0ec300f718c57cb279ece8
Arcensoth <arcensoth@gmail.com>
2017-12-08 02:58:47 -0500

    Remove unused test

--- tests/selector/test_selector_arguments.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-from pymcutil.selector.selector_arguments import SelectorArguments
-
-
-class SelectorArgumentsTestCase(unittest.TestCase):
-    def test(self):
-        args = SelectorArguments(limit=5)
-        print(args.limit)
