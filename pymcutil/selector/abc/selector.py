06b445b0686bbd67a274bb31c1576897b84d57d7
Arcensoth <arcensoth@gmail.com>
2018-07-01 18:35:51 -0400

    Finish selector stringification in a way that is very unsatisfying

--- pymcutil/selector/abc/selector.py
+++ pymcutil/selector/abc/selector.py
@@ -104,8 +104,79 @@ class Selector(MappingSiftable):
         self.limit: Optional[int] = nullable_int(limit)
 
     def __str__(self):
-        # TODO selector stringify
-        return 'TODO'
+        arguments = tuple(self.arguments)
+        argument_str = ''.join(('[', ','.join(['{}={}'.format(k, v) for k, v in arguments]), ']')) if arguments else ''
+        return '@{}{}'.format(self.base, argument_str or '')
+
+    @property
+    def arguments(self) -> Iterable[Tuple[str, str]]:
+        # TODO This is disguesting. Has it always been this way?
+        if self.x is not None:
+            yield 'x', str(self.x)
+        if self.y is not None:
+            yield 'y', str(self.y)
+        if self.z is not None:
+            yield 'z', str(self.z)
+
+        if self.dx is not None:
+            yield 'dx', str(self.dx)
+        if self.dy is not None:
+            yield 'dy', str(self.dy)
+        if self.dz is not None:
+            yield 'dz', str(self.dz)
+
+        if self.x_rotation is not None:
+            yield 'x_rotation', str(self.x_rotation)
+        if self.y_rotation is not None:
+            yield 'y_rotation', str(self.y_rotation)
+
+        if self.distance is not None:
+            yield 'distance', str(self.distance)
+
+        if self.level is not None:
+            yield 'level', str(self.level)
+
+        if self.type_ is not None:
+            yield 'type', str(self.type_)
+        if self.not_types is not None:
+            yield from (('type', '!{}'.format(type_)) for type_ in self.not_types)
+
+        if self.name is not None:
+            yield 'name', str(self.name)
+        if self.not_names is not None:
+            yield from (('name', '!{}'.format(name)) for name in self.not_names)
+
+        if self.team is not None:
+            yield 'team', str(self.team)
+        if self.not_teams is not None:
+            yield from (('team', '!{}'.format(team)) for team in self.not_teams)
+
+        if self.gamemode is not None:
+            yield 'gamemode', str(self.gamemode)
+        if self.not_gamemodes is not None:
+            yield from (('gamemode', '!{}'.format(gamemode)) for gamemode in self.not_gamemodes)
+
+        if self.tags is not None:
+            yield from (('tag', '{}'.format(tag)) for tag in self.tags)
+        if self.not_tags is not None:
+            yield from (('tag', '!{}'.format(tag)) for tag in self.not_tags)
+
+        if self.nbts is not None:
+            yield from (('nbt', '{}'.format(nbt)) for nbt in self.nbts)
+        if self.not_nbts is not None:
+            yield from (('nbt', '!{}'.format(nbt)) for nbt in self.not_nbts)
+
+        if self.scores is not None:
+            yield 'scores', str(self.scores)
+
+        if self.advancements is not None:
+            yield 'advancements', str(self.advancements)
+
+        if self.sort is not None:
+            yield 'sort', str(self.sort)
+
+        if self.limit is not None:
+            yield 'limit', str(self.limit)
 
     @property
     @abc.abstractmethod
