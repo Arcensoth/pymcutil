7c164dbcfd495985b55ae62b227be436d0f81ae7
Arcensoth <arcensoth@gmail.com>
2018-07-01 20:19:39 -0400

    Add more information to setup.py

--- setup.py
+++ setup.py
@@ -17,6 +17,16 @@ setup(
     url='https://github.com/Arcensoth/pymcutil',
     license='MIT',
     description='An expressive Minecraft utility library revolving around data manipulation and generation.',
+    long_description_content_type='text/markdown',
     long_description=readme,
     packages=find_packages(),
-    install_requires=requirements)
+    install_requires=requirements,
+    python_requires='>=3.6',
+    keywords='minecraft commands library utility datapack data',
+    classifiers=(
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+    ))
